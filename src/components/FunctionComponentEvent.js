7db33b93005309f3f28d0b02daf5b57960997bff
RezaulAlamOni <rzalam93@gmail.com>
2023-01-22 00:46:54 +0600

    Update class base componenents

--- src/components/FunctionComponentEvent.js
+++ src/components/FunctionComponentEvent.js
@@ -1,15 +1,20 @@
+import {useState} from "react";
 function FunctionComponentEvent() {
+    const [counter, setCounter] = useState(0);
+
     function handleCLick() {
         console.log('Handle CLick')
     }
-
     const handleClickF = ()=>{
-        console.log('Handle CLick 5554')
+        setCounter(counter+1 )
     }
+
     return (
         <div>
             <h2>Function Component </h2>
             <button onClick={handleCLick}>Click</button>
+
+            <h2>Show Counter {counter}</h2>
             <button onClick={handleClickF}>Click For count</button>
         </div>
     )
